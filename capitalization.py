5e3c8a22e1cac88fd5bd55fc06f433ac58f6f6e6
MonikaPdb <34090470+MonikaPdb@users.noreply.github.com>
2018-01-05 15:15:26 +0100

    Update capitalization.py

--- capitalization.py
+++ capitalization.py
@@ -1,12 +1,15 @@
-def kapitalizace():
+    
+def mycapitalize():
     import re
-    zadani = input('Napis vetu. Veta muze obsahovat pouze mezery a mala pismena, bez diaktiriky. Tva veta: ')
-    vysledek = ''
-    while not re.match("^[a-z ]*$", zadani):
-        zadani = input('Zadani bylo spatne, zkus to znova. Tva veta: ')
-    if re.match("^[a-z ]*$", zadani): 
-        rozsekani = zadani.split()
-        kapitalizace = [word[0].upper() + word[1:] for word in rozsekani]
-        vysledek = ' '.join(kapitalizace)
-        print('Vetu si zadal dobre. Zde je vysledek funkce:', vysledek)
-kapitalizace()
+    print('The function will capitalize the first character in every word in a given sentence.')
+    sentence = input('Insert a sentence. Sentence can only contain characters a-z and space and cannot be blank or only spaces. Insert your sentence: ')
+    result = ''
+    while not re.match("^[a-z ]*$", sentence) or (not sentence or sentence.isspace()):
+        sentence = input('Your input was not valid. Try again: ')
+    if re.match("^[a-z ]*$", sentence): 
+        splitting = sentence.split()
+        cap = [word[0].upper() + word[1:] for word in splitting]
+        result = ' '.join(cap)
+        print('Your input was valid. Here is the sentence:', result)
+        
+mycapitalize()
