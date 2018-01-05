a6e606529c19fef59381cf5b4e7b90d75420026e
MonikaPdb <34090470+MonikaPdb@users.noreply.github.com>
2018-01-05 15:27:43 +0100

    Update Factorial.py

--- Factorial.py
+++ Factorial.py
@@ -1,19 +1,20 @@
-def faktorial(): 
+def myfactorial(): 
     while True: 
         try:
-            cislo=int(input('Vloz cele kladne cislo, funkce vypise jeho faktorial. Cislo: '))
+            number = int(input('Insert whole positive number, the function will return its factorial. Insert number: '))
             if True:
-                 vysledek = 1
-                 while cislo < 0:
-                     print('Podarilo se ti zadat cele cislo. Bohuzel ale nebylo kladne. Zkus to znova.')
-                     cislo=int(input('Vloz cele cislo, funkce vypise jeho faktorial. Cislo: '))            
-                 while cislo >= 1:
-                     vysledek = vysledek * cislo
-                     cislo = cislo - 1
-                 print('Faktorial je', vysledek)
+                 result = 1
+                 while number < 0:
+                     print('You inserted whole negative number. Try again: .')
+                     number = int(input('Insert whole positive number, the function will return its factorial. Insert number: '))            
+                 while number >= 1:
+                     result = result * number
+                     number = number - 1
+                 print('Factorial is', result)
                  break
         except ValueError:
-            print('Nevlozil/a jsi cele kladne cislo. Zkus to znova.')
+            print('You have not inserted whole positive number. Try again: ')
             pass            
     
-faktorial()
+myfactorial()
+
